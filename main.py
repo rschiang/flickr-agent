b2a35db9456c33d9d95d50601cbbba76c166a051
Poren Chiang <ren.chiang@gmail.com>
2015-04-12 22:52:43 +0800

    Sort files base on EXIF information

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+import exifread
 import flickr_api as flickr
 import json
 import os
@@ -14,10 +16,27 @@ def load_settings():
         flickr.set_auth_handler(handler)
     return settings
 
+def guess_time(path):
+    with open(path, 'rb') as f:
+        dates = []
+
+        tags = exifread.process_file(f)     # EXIFRead won't throw errors
+        for tag in ('EXIF DateTimeOriginal', 'EXIF DateTimeDigitized', 'Image DateTime'):
+            if tag in tags:
+                try:
+                    dates.append(datetime.strptime(tags[tag], '%Y:%m:%d %H:%M:%S'))
+                except ValueError: pass
+
+        if not dates:
+            return min(os.path.getctime(path), os.path.getmtime(path))
+        else:
+            return min(dates)
+
 def upload():
     upload_dir = settings.get('upload_dir', 'upload')
     success_dir = settings.get('success_dir', 'success')
-    for file in os.listdir(upload_dir):
+    files = sorted(os.listdir(upload_dir), key=guess_time)
+    for file in files:
         try:
             path = os.path.join(upload_dir, file)
             content_type = 2 if file.lower().endswith('.png') else 1
